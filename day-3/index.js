f17eb60dc65cdfe50214c6f8bbc434727c358688
Calum Smith <hello@calum.io>
2020-12-03 00:20:44 -0500

    Day 3 touchups

--- day-3/index.js
+++ day-3/index.js
@@ -4,19 +4,18 @@ const fs = require('fs');
 const input = fs.readFileSync(0, 'utf8');
 const landscape = input.trim().split("\n").map(String);
 
-let width = landscape[0].length;
+const width = landscape[0].length;
 
 function traverse(dx, dy) {
   let count = 0;
   for (let y = 0, x = 0; y < landscape.length; y += dy) {
-    const spot = landscape[y][x];
-    if (spot === "#") count++;
+    if (landscape[y][x] === "#") count++;
     x = (x + dx) % width;
   }
   return count;
 }
 
-let secondTrees = traverse(1, 1) * traverse(3, 1) * traverse(5, 1) * traverse(7, 1) * traverse(1, 2);
+const secondTrees = traverse(1, 1) * traverse(3, 1) * traverse(5, 1) * traverse(7, 1) * traverse(1, 2);
 
 
 console.log(`ANSWER 1: ${traverse(3, 1)}`);
